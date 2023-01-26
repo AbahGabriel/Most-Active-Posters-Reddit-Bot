5f4b72f1cfe1acdae74751ffb3830557140cbd1e
Abah Olotuche Gabriel <105561324+AbahGabriel@users.noreply.github.com>
2023-01-26 21:44:34 +0100

    Update Most_Active_Posters_Flair_Updater.py

--- Most_Active_Posters_Flair_Updater.py
+++ Most_Active_Posters_Flair_Updater.py
@@ -1,98 +1,104 @@
 import praw
-from prawcore.exceptions import Forbidden
+import itertools
 import datetime
+import prawcore.exceptions
 import os
 import re
 
-#  Connects to Reddit's API and creates a Reddit object
-#  which allows me to perform various actions
-reddit = praw.Reddit('MostActivePosters')
+#Ensure that this bot is a moderator, or it won't work
+def main():
+    #File path
+    path = "C:\\Users\\Gabriel\\source\\repos\\API_Practice\\TestTopPosters\\test.txt"
 
-#Gets info
-subreddit_name = input("Enter subreddit name: ")
+    #Go to subreddit
+    reddit = praw.Reddit('MostActivePosters')
+    subreddit_name = 'CelebsWithPetiteTits'
+    subreddit = reddit.subreddit(subreddit_name)
+    
+    #Retrieve all posts made within the past week
+    lastWeekPosts = getPostsMadeWithinPastWeek(subreddit)
 
-#Creates subreddit instance of specified subreddit
-subreddit = reddit.subreddit(subreddit_name)
+    #Get author of each post and assign them a numberOfPosts value
+    authorsAndPosts = setPostNumberForEachAuthor(lastWeekPosts)
 
-#Retrieves all new posts from subreddit
-posts = subreddit.new()
-timeRange = datetime.timedelta(days=7)
-filteredPosts = []
-for post in posts:
-    #Checks if post was created between the current time and the specified range
-    post_creation_date = datetime.datetime.fromtimestamp(post.created_utc)
-    if (post_creation_date > (datetime.datetime.today() - timeRange)):
-        filteredPosts.append(post)
-    else:
-        break
+    #Deletes flairs used for previous week
+    #Had to place it here to allow it to accept authorsAndPosts as an argument
+    deletePreviousFlairs(subreddit, reddit, path, authorsAndPosts)
 
-#Keeps track of how many posts each user made within the specified range
-dictionary = {}
+    #Filter out authors who currently have flairs
+    authorsAndPosts = removeFlairedAuthors(authorsAndPosts, lastWeekPosts)
 
-#Tries to filter out users with flairs
-try:
-    flairsText = []
-    flairs = []
-    linkFlairs = []
+    #Sorts dictionary in descending order
+    authorsAndPosts = sortDictionaryInDescendingOrder(authorsAndPosts)
 
-    #Retrieves all user flairs
-    for flair in subreddit.flair.templates:
-        flairsText.append(flair['text'])
-        flairs.append(flair)
+    #Keeps top 5 Users
+    authorsAndPosts = keepTopFivePosters(authorsAndPosts)
 
-    #Retrieves all post flairs
-    for flair in subreddit.flair.link_templates:
-        linkFlairs.append(flair)
+    #Create five new flairs labelled: "#{num} Poster for Week {currentDay}"
+    #Note - Account must be a moderator for this part
+    createdFlairs = createNewFlairs(subreddit)
 
-    for post in filteredPosts:
-        if post.author_flair_text not in flairsText:
-            dictionary[post.author] = []
+    #Sets user flairs
+    setUserFlairs(authorsAndPosts, createdFlairs, subreddit)
 
-    #Uses list to keep track of all user's posts
-    for post in filteredPosts:
-        for key in dictionary:
-            if (post.author == key):
-                dictionary[key].append(post)
+    #Saves the newly created flairs
+    saveNewFlairs(path, createdFlairs)
 
-#Default approach where all users are considered
-except Forbidden:
+def getPostsMadeWithinPastWeek(subreddit):
+    #All new posts in the subreddit
+    allNewPosts = subreddit.new()
 
-    for post in filteredPosts:
-        dictionary[post.author] = []
-
-    #Uses list to keep track of all user's posts
-    for post in filteredPosts:
-        for key in dictionary:
-            if (post.author == key):
-                dictionary[key].append(post)
-
-#Deletes flairs from previous week
-filename = "C:\\Users\\Gabriel\\source\\repos\\API_Practice\\Top_Posters_Bot\\topposters.txt"
-redditors = []
-flairs = []
-if os.path.exists(filename) == True:
-    with open(filename, 'r') as f:
-        if "," in f:
-            for line in f:
-                line = line.split(',')
-                flairs.append(line[1])
-
-            for flair in flairs:
-                subreddit.flair.templates.delete(flair['id'])
+    #Posts within past week
+    timeRange = datetime.timedelta(days=7)
+    filteredPosts = []
+    for post in allNewPosts:
+        #Checks if post was created between the current time and the past week
+        post_creation_date = datetime.datetime.fromtimestamp(post.created_utc)
+        if (post_creation_date > (datetime.datetime.today() - timeRange)):
+            filteredPosts.append(post)
         else:
-            for line in f:
-                subreddit.flair.delete(line)
+            break
 
-        #Clears file
-        open(filename, "w").truncate(0)
-else:
-    #Creates file
-    open(filename, "w").close()
+    return filteredPosts
 
+def setPostNumberForEachAuthor(lastWeekPosts):
+    dictionary = {}
 
-#Creates weekly flairs
-newFlairs = []
-try:
+    #Counts number of posts for each author
+    for post in lastWeekPosts:
+        if post.author in dictionary:
+            dictionary[post.author] += 1
+        else:
+            dictionary[post.author] = 1
+    
+    return dictionary
+
+def removeFlairedAuthors(authorDictionary, lastWeekPosts):
+    #Check if any author currently has a flair
+    for post in lastWeekPosts:
+        if post.author_flair_text != (None or ''):
+            try:
+                del authorDictionary[post.author] #Remove if yes
+            except KeyError: #Handles duplicate posters
+                continue
+    
+    return authorDictionary
+    
+def sortDictionaryInDescendingOrder(dictionary):
+    #Sorts dictionary according to the values (x[1])
+    dictionary = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
+    return dictionary
+
+def keepTopFivePosters(authorsAndPosts):
+    # Converts dict to tuple
+    # Get first N items in dictionary
+    authorTuple = tuple(authorsAndPosts)
+    topFivePosters = dict(list(authorTuple)[0: 5]) 
+
+    return topFivePosters
+
+def createNewFlairs(subreddit):
+    #Creates five new flairs that can only be assigned by moderators
     for i in range(1, 6):
         text = f'#{i} Poster for Week {datetime.datetime.today()}'
         text = re.sub("\..*$","",text)
@@ -100,67 +106,54 @@ try:
         allowable_content='text',
         mod_only=True,
         text_editable=False)
-
+    
     #Tracks created flairs for future identification
+    newFlairs = []
     for flair in subreddit.flair.templates:
         if "Poster for Week" in flair['text']:
             newFlairs.append(flair)
-    newFlairs.sort(key=lambda x: x['text'], reverse=False)
+    newFlairs = sorted(newFlairs, key=lambda i: i['text'])
 
-except Forbidden:
-    print("Bot does not have authority to create flairs.")
+    return newFlairs
 
-#Prints out users and the number of their posts
-sortedDict = dict(sorted(dictionary.items(), key=lambda item: len(item[1]), reverse=True))
-topposters = ""
-num = 1
-for key in sortedDict:
-    if num <= 5:
-        try:
-            try:
-                #Sets flairs of corresponding users
-                if len(newFlairs) != 0:
-                    text = f'#{i} Poster for Week {datetime.datetime.today()}'
-                    text = re.sub("\..*$","",text)
-                    subreddit.flair.set(f"{key.name}", text=text,flair_template_id=newFlairs[num]['id'])
-            except Forbidden:
-                print("Bot does not have authority to set flairs.")
+def setUserFlairs(authorsAndPosts, createdFlairs, subreddit):
+    #Sets flairs for top 5 posters, in order
+    i = 0
+    for author in authorsAndPosts:
+        if i > 5:
+            break
+        text = f'#{i+1} Poster for Week {datetime.datetime.today()}'
+        text = re.sub("\..*$","",text),
+        subreddit.flair.set(author.name, text=text,flair_template_id=createdFlairs[i]['id'])
 
-            topposters += f"{num}. {key.name}: {len(dictionary[key])} posts\n\n"
-            #Stores names and ids in file to be deleted in the next week
-            with open(filename, 'a') as f:
-                if len(newFlairs) != 0:
-                    f.write(f"{key},{newFlairs[num]}\n")
-                else:
-                    f.write(f"{key}\n")
+def saveNewFlairs(path, createdFlairs):
+    if not os.path.exists(path):
+        open(path, "w").close()
 
-                num += 1
-        except AttributeError:
-            print("Invalid user. Skipping.")
-    else:
-        break
+    for flair in createdFlairs:
+        with open(path, 'a') as openFile:
+            openFile.write(f'{flair}\n')
+
+def deletePreviousFlairs(subreddit, reddit, path, authorsAndPosts):
+    if not os.path.exists(path) or os.stat(path).st_size == 0:
+        return
     
-if len(linkFlairs) != 0:
-    subreddit.submit(title="Top Posters Of The Week",selftext=topposters,flair_id=linkFlairs[0]['id'])
-else:
-    subreddit.submit(title="Top Posters Of The Week",selftext=topposters)
+    #Clears user flairs
+    userList = []
+    for author in authorsAndPosts:
+        userList.append(author)
+    deleteUserFlairs(userList, subreddit)
 
-""" subreddit = reddit.subreddit("RandomTest123")
+    #Deletes flairs from subreddit
+    with open(path, 'r') as file_in:
+        for line in file_in: #Each line is a flair
+            subreddit.flair.templates.delete(line[158:194])
 
- Deletes flairs based on id
-for flair in subreddit.flair.templates:
-    subreddit.flair.templates.delete(flair['id'])
+    #Clears file
+    with open(path, 'r+') as file_in:
+        file_in.truncate(0)
+           
+def deleteUserFlairs(userList, subreddit):
+    subreddit.flair.update(userList, text='')
 
-    for flair in flairs:
-                subreddit.flair.templates.delete(flair['id'])
-
- Removes flair from user
-for post in subreddit.new():
-    redditor = post.author
-    subreddit.flair.delete(redditor)
-
-for flair in subreddit.flair.templates:
-    print(flair['text']) """
-
-
-exit()
+main()
