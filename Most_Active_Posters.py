6fb1b672771d26161d3e655cea0882523df2622a
Abah Olotuche Gabriel <105561324+AbahGabriel@users.noreply.github.com>
2023-01-28 14:24:41 +0100

    Update Most_Active_Posters.py

--- Most_Active_Posters.py
+++ Most_Active_Posters.py
@@ -1,44 +1,87 @@
 import praw
 import datetime
+import os
 
-#Connects to Reddit's API and creates a Reddit object
-#which allows me to perform various actions
-reddit = praw.Reddit('MostActivePosters')
+#This bot can function without being a moderator
 
-#Gets info
-subreddit_name = input("Enter subreddit name: ")
-time_range = float(input("Time period (in days): "))
-
-#Creates subreddit instance of specified subreddit
-subreddit = reddit.subreddit(subreddit_name)
-
-#Retrieves all new posts from subreddit
-posts = subreddit.new()
-timeRange = datetime.timedelta(days=time_range) #Sets a time range of 7 days
-filteredPosts = []
-for post in posts:
-    #Checks if post was created between the current time and 7 days ago
-    post_creation_date = datetime.datetime.fromtimestamp(post.created_utc)
-    if (post_creation_date > (datetime.datetime.today() - timeRange)):
-        filteredPosts.append(post)
-    else:
-        break
-
-#Keeps track of how many posts each user made within the past week
-#Creates a list for each user
-dictionary = {}
-for post in filteredPosts:
-    dictionary[post.author] = []
-
-#Uses list to keep track of all user's posts
-for post in filteredPosts:
-    for key in dictionary:
-        if (post.author == key):
-            dictionary[key].append(post)
-
-#Prints out users and the number of their posts
-sortedDict = dict(sorted(dictionary.items(), key=lambda item: len(item[1]), reverse=True))
-for key in sortedDict:
-    print(f"{key.name}: {len(dictionary[key])} posts")
+#Grabs Top N Posters of the Week and PMs a moderator with their usernames
+def main():
+    #Go to subreddit
+    reddit = praw.Reddit('MostActivePosters')
+    subreddit_name = 'babesfrom1996'
+    subreddit = reddit.subreddit(subreddit_name)
     
-input()
+    #Retrieve all posts made within the past week
+    lastWeekPosts = getPostsMadeWithinPastWeek(subreddit)
+
+    #Get author of each post and assign them a numberOfPosts value
+    authorsAndPosts = setPostNumberForEachAuthor(lastWeekPosts)
+
+    #Sorts dictionary in descending order
+    authorsAndPosts = sortDictionaryInDescendingOrder(authorsAndPosts)
+
+    #Keeps top N Users
+    authorsAndPosts = keepTopNPosters(authorsAndPosts)
+
+    #PMs mod with Top N Posters' names
+    sendPMToMod(authorsAndPosts, reddit, subreddit_name)
+
+def getPostsMadeWithinPastWeek(subreddit):
+    #All new posts in the subreddit
+    allNewPosts = subreddit.new()
+
+    timeRange = datetime.timedelta(days=7)
+    filteredPosts = []
+    for post in allNewPosts:
+        #Checks if post was created between the current time and the past week
+        post_creation_date = datetime.datetime.fromtimestamp(post.created_utc)
+        if (post_creation_date > (datetime.datetime.today() - timeRange)):
+            filteredPosts.append(post)
+        else:
+            break
+
+    return filteredPosts
+
+def setPostNumberForEachAuthor(lastWeekPosts):
+    dictionary = {}
+
+    print("Getting posts from past week...")
+    #Counts number of posts for each author
+    for post in lastWeekPosts:
+        if post.author in dictionary:
+            dictionary[post.author] += 1
+        else:
+            dictionary[post.author] = 1
+    
+    return dictionary
+    
+def sortDictionaryInDescendingOrder(dictionary):
+    print("Sorting users based on post count...")
+    #Sorts dictionary according to the values (x[1])
+    dictionary = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
+    return dictionary
+
+def keepTopNPosters(authorsAndPosts):
+    # Converts dict to tuple
+    # Get first N items in dictionary
+    authorTuple = tuple(authorsAndPosts)
+    topNPosters = dict(list(authorTuple)[0: 5]) 
+
+    return topNPosters
+
+def sendPMToMod(authorsAndPosts, reddit, subreddit_name):
+    modName = ""
+
+    #Constructs subject content
+    subject = f"r/{subreddit_name}: Top Posters Of The Week"
+
+    #Constructs message content
+    message = ""
+    i = 1
+    for author in authorsAndPosts:
+        message += f"{i}. {author}: {authorsAndPosts[author]} posts\n"
+        i += 1
+    
+    reddit.redditor(modName).message(subject=subject, message=message)
+
+main()
