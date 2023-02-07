f3e8d702568417b8f15904344a90bbc2bc6ca271
Abah Olotuche Gabriel <105561324+AbahGabriel@users.noreply.github.com>
2023-02-07 19:36:49 +0100

    Update Most_Active_Posters_Flair_Updater.py

--- Most_Active_Posters_Flair_Updater.py
+++ Most_Active_Posters_Flair_Updater.py
@@ -14,7 +14,7 @@ def main():
 
     #Go to subreddit
     reddit = praw.Reddit('MostActivePosters')
-    subreddit_name = 'CelebsInJeanShorts'
+    subreddit_name = 'test'
     subreddit = reddit.subreddit(subreddit_name)
 
     #Removes flairs from previous week's flair awardees
